81ef5f11b4518f99d5d24a3efa5845222cf3da03
Kamal Chaudhari <kamalchaudhary502@gmail.com>
2024-07-21 23:14:35 +0530

    Embedded H2 database for Dynamic data genration

--- src/main/java/com/project/microservices/currency_exchange_service/Beans/CurrencyExchange.java
+++ src/main/java/com/project/microservices/currency_exchange_service/Beans/CurrencyExchange.java
@@ -1,14 +1,21 @@
 package com.project.microservices.currency_exchange_service.Beans;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 
-
+@Entity
 public class CurrencyExchange {
 
+    @Id
     private Long id;
 
+    @Column(name = "currency_from")
     private String from;
 
+    @Column(name = "currency_to")
     private String to;
 
     private BigDecimal conversionMultiple;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT into currency_exchange
+(id, currency_from, currency_to,conversion_multiple,environment)
+values(1001, 'USD','INR',80 ,'');
+Insert into currency_exchange
+(id, currency_from, currency_to,conversion_multiple, environment)
+values(1002, 'EUR','INR',90 ,'');
+INSERT into currency_exchange
+(id, currency_from, currency_to,conversion_multiple, environment)
+values(1003, 'AUD','INR',25 ,'');
+INSERT into currency_exchange
+(id, currency_from, currency_to,conversion_multiple, environment)
+values(1004, 'KRW','INR',5 ,'');
+INSERT into currency_exchange
+(id, currency_from, currency_to,conversion_multiple, environment)
+values(1005, 'HKP','INR',20 ,'');
